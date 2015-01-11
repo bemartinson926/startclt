db02b3d5cf6a56c3d365bf13af0da823069e71cb
Matthew T. Banbury <matbanbury@gmail.com>
2015-01-11 11:19:51 -0500

    Adds assocations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  rolify
+  has_many :memberships
+  has_many :groups, through: :memberships
+  has_many :rsvps
+  has_many :events, through: :rsvps
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
