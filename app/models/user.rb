3e6c761cde8d009c8daf8a1a1368ded810567347
Zachary Kirchin <zacharykirchin@gmail.com>
2015-01-13 16:51:49 -0500

    add avatar_url attribute, and email methods in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  before_validation :prep_email
+  before_save :create_avatar_url
+
   has_many :memberships
   has_many :groups, through: :memberships
   has_many :rsvps
@@ -8,4 +11,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	private
+
+	def prep_email
+		self.email = self.email.strip.downcase if self.email
+	end
+
+	def create_avatar_url
+		self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+	end
+
 end
