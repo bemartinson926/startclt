eaac377d56fbae17f0ffaa6ad146ac1402d902cd
Blake Martinson <blake.martinson@gmail.com>
2015-01-15 00:18:07 -0500

    Add specs for Rsvp

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Rsvp, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "has a valid factory" do
+    subject { FactoryGirl.build(:rsvp) }
+    it { is_expected.to be_valid }
+  end
+
+  context "associations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:event) }
+  end
 end
