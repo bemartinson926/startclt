29d3496b21eaa6a4e8e945a388bd35f77cdf2ac6
Blake Martinson <blake.martinson@gmail.com>
2015-01-14 22:54:13 -0500

    Add specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   context "validation" do
-    subject{ build(:user) }
-    it{ should be_valid }
+    subject { build(:user) }
+    it { is_expected.to be_valid }
+  end
+
+  context "invalid user" do
+    subject { build(:user, email: nil) }
+    it { is_expected.to be_invalid }
+  end
+
+  context "associations" do
+    it { is_expected.to have_many(:memberships) }
+    it { is_expected.to have_many(:groups) }
+    it { is_expected.to have_many(:rsvps) }
+    it { is_expected.to have_many(:events) }
   end
 end
