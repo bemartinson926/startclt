d4ac0f9e679cca256172cab54a18605b5423f6ca
Matthew T. Banbury <matbanbury@gmail.com>
2015-01-17 12:46:31 -0500

    Tests existing user login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
-describe "Logging in", :type => :feature do
+feature 'User signs in' do
+  background do
+    @user = create(:user)
+  end
+
+  scenario "with existing username" do 
+    visit root_path
+    click_link "Sign In"
+    within(:css , "#login") do
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: @user.password
+      click_button "Log in"
+    end
+    expect(page).to have_content "Welcome, #{@user.first_name}"
+    # page.should have_content "Welcome, #{@user.first_name}"
+  end
+
+
+# describe "Logging in", :type => :feature do
   
   # let(:user){ create(:user) }
 
@@ -15,6 +33,20 @@ describe "Logging in", :type => :feature do
 
   #   expect(page).to have_content 'Signed in successfully.'
   # end
+
+#   let(:user){ create(:user) }
+
+#   it "signs me in" do
+#     visit new_user_session_path
+#     within("#new_user") do
+#       fill_in 'Email', :with => user.email
+#       fill_in 'Password', :with => '12345678'
+#       click_button 'Log in'
+#     end
+#     #save_and_open_page
+
+#     expect(page).to have_content 'Signed in successfully.'
+#   end
   
   # it "shows me my email in the navigation" do
   #   visit new_user_session_path
