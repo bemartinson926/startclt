f4d8b5a73f39950216525d4c109d1b9928be0a81
Matthew T. Banbury <matbanbury@gmail.com>
2015-01-17 14:44:52 -0500

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113214017) do
+ActiveRecord::Schema.define(version: 20150117191902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150113214017) do
     t.datetime "ends_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "groups", force: true do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150113214017) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "membership_roles", force: true do |t|
