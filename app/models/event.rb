2c1a5d5cabd15e602a223913348cadb45e679609
Matthew T. Banbury <matbanbury@gmail.com>
2015-01-17 14:41:24 -0500

    Includes sluggable module, adds sluggable column

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,11 @@
 class Event < ActiveRecord::Base
+  include Sluggable
+
   belongs_to :group
   has_many :rsvps
   has_many :users, through: :rsvps
 
   validates :name, presence: true
+
+  sluggable_column :name
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,11 @@
 class Group < ActiveRecord::Base
+  include Sluggable
+
   has_many :memberships
   has_many :users, through: :memberships
   has_many :events
 
   validates :name, presence: true
+
+  sluggable_column :name
 end
