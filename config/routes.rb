735d61ef3d9c5ce1054f2fa1368566cf2e5eaaf5
Sharon Hunter <sharonhunter@runbox.com>
2015-01-17 16:33:35 -0500

    Working on routes for dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   resources :events
 
   get '/new_group' => 'groups#new_group', as: 'first_group'
-  get '/dashboard/:id', to: 'groups#group_dashboard', as: 'group_dashboard'
+  get '/group/dashboard/:id', to: 'groups#group_dashboard', as: 'group_dashboard'
   resources :groups
 
   root to: 'welcome#home'
   get 'welcome/home'
   get '/about' => 'welcome#about'
 
+  get '/user/dashboard/:id', to: 'users#user_dashboard', as: 'user_dashboard'
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
