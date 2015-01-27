fc3c12d8d4f8e49abc4263fd3321a1a8883b03fa
Matthew T. Banbury <matbanbury@gmail.com>
2015-01-26 22:36:47 -0500

    Adds simple authentication with access_denied method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   	"/dashboard/user/#{current_user.slug}"
   end
+
+  def access_denied
+    flash[:danger] = "You don't have permission to do that."
+    redirect_to :back
+  end
 end
