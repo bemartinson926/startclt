664364a19454cfd388453b46af6a85dbf8825fd3
Sharon Hunter <sharonhunter@runbox.com>
2015-01-30 21:00:41 -0500

    Adds working third-party web fonts and associated CSS

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module Startclt
     
     config.time_zone = 'Eastern Time (US & Canada)'
 
+    #this line added to allow asset pipeline to use webfonts
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
