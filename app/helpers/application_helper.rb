0549c99f46fff4ce8491284ba323ba5593c519d1
Matthew T. Banbury <matbanbury@gmail.com>
2015-02-02 08:21:36 -0500

    Makes event datetimes human-friendly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def pretty_datetime(dt)
+    # Change to Eastern Time, because Charlotte
+    dt = dt.in_time_zone("Eastern Time (US & Canada)")
+    
+    # "01/24/2015 at 11:05am"
+    # dt.strftime("%m/%d/%Y at %l:%M%P")
+
+    # "Saturday, Jan. 24 at 11:05 AM"
+    dt.strftime("%A, %b. %d at %l:%M%P")
+  end
+
 end
