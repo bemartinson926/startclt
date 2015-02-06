fc84b1c708e52e37798a037bb8db4a90b86fff70
Blake Martinson <blake.martinson@gmail.com>
2015-02-05 22:21:06 -0500

    Set role to Organizer when user creates group

--- app/models/group.rb
+++ app/models/group.rb
@@ -15,6 +15,12 @@ class Group < ActiveRecord::Base
   private
 
     def set_membership
-      Membership.create!(user_id: self.user_id, group_id: self.id)
+      @membership = Membership.create!(user_id: self.user_id, group_id: self.id)
+      set_organizer_membership_role(@membership)
     end
+
+    def set_organizer_membership_role(membership)
+      @role = MembershipRole.new(membership_id: membership.id, role: 1)
+      @role.save!
+    end    
 end
