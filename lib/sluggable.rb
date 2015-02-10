1e9b5c6d9024b50f8005f57a2e638b587526a783
Matthew T. Banbury <matbanbury@gmail.com>
2015-02-10 13:39:17 -0500

    Fixes non-unique slug id error

--- lib/sluggable.rb
+++ lib/sluggable.rb
@@ -11,8 +11,23 @@ module Sluggable
   end
 
   def generate_slug
-    the_slug = self.send(self.class.slug_column)
-    self.slug = the_slug.parameterize
+    the_slug = self.send(self.class.slug_column.to_sym).parameterize
+    obj = self.class.find_by slug: the_slug
+    count = 2
+    while obj && obj != self
+      the_slug = append_suffix(the_slug, count)
+      obj = self.class.find_by slug: the_slug
+      count += 1 
+    end
+    self.slug = the_slug
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
   end
 
   module ClassMethods
